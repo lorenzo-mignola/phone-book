5cce54dc3209929c02d49694c3cf4b6b5c3ac58e
lorenzo-mignola <lo.mignola@gmail.com>
2026-08-04 10:38:03 +0200

    feat: add endpoint to retirve 1 contact

--- src/repository/contacts.rs
+++ src/repository/contacts.rs
@@ -5,12 +5,30 @@ use crate::entity::phone_numbers;
 use crate::error::AppError;
 use crate::repository::contact_with_numbers::ContactWithNumbers;
 
-pub async fn find_all(db: &DatabaseConnection) -> Result<Vec<ContactWithNumbers>, AppError> {
-    let contacts = contacts::Entity::find()
+pub(crate) async fn find_all(db: &DatabaseConnection) -> Result<Vec<ContactWithNumbers>, AppError> {
+    let contact_with_numbers = contacts::Entity::find()
         .find_with_related(phone_numbers::Entity)
         .all(db)
         .await
-        .map_err(AppError::Db)?;
+        .map_err(AppError::Db)?
+        .into_iter()
+        .map(ContactWithNumbers::from)
+        .collect();
 
-    Ok(contacts.into_iter().map(ContactWithNumbers::from).collect())
+    Ok(contact_with_numbers)
+}
+
+pub(crate) async fn find_by_id(
+    db: &DatabaseConnection,
+    id: i32,
+) -> Result<ContactWithNumbers, AppError> {
+    contacts::Entity::find_by_id(id)
+        .find_with_related(phone_numbers::Entity)
+        .all(db)
+        .await
+        .map_err(AppError::Db)?
+        .into_iter()
+        .next()
+        .map(ContactWithNumbers::from)
+        .ok_or(AppError::NotFound)
 }
--- src/routes/contacts.rs
+++ src/routes/contacts.rs
@@ -1,9 +1,15 @@
-use axum::{Json, Router, extract::State, routing::get};
+use axum::{
+    Json, Router,
+    extract::{Path, State},
+    routing::get,
+};
 
 use crate::{dto::contact_dto::ContactDto, error::AppError, repository, state::AppState};
 
 pub(super) fn contacts_router() -> Router<AppState> {
-    Router::new().route("/contacts", get(list_contacts))
+    Router::new()
+        .route("/contacts", get(list_contacts))
+        .route("/contacts/{id}", get(get_contact))
 }
 
 async fn list_contacts(State(state): State<AppState>) -> Result<Json<Vec<ContactDto>>, AppError> {
@@ -11,4 +17,13 @@ async fn list_contacts(State(state): State<AppState>) -> Result<Json<Vec<Contact
 
     let contacts_dto = contacts.into_iter().map(ContactDto::from).collect();
     Ok(Json(contacts_dto))
+}
+
+async fn get_contact(
+    Path(id): Path<i32>,
+    State(state): State<AppState>,
+) -> Result<Json<ContactDto>, AppError> {
+    let contact = repository::contacts::find_by_id(&state.db, id).await?;
+
+    Ok(Json(ContactDto::from(contact)))
 }
